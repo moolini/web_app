d666145caa329479311087924fc98b1b95900c3f
moolini <50342815+moolini@users.noreply.github.com>
2020-09-20 17:41:56 -0700

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import random 
+from _collections import defaultdict
+import pandas as pd
+import requests
+
+
+
+Nick_text = pd.read_pickle('Splice_In_Time.pkl')
+Shakespeare = pd.read_pickle("Shakespeare.pkl")
+
+def markov_chain(text):
+    words = text.split()
+    dictionary = defaultdict(list)
+    for current_word, next_word in zip(words[0:], words[1:]):
+        dictionary[current_word].append(next_word)
+    dictionary = dict(dictionary)
+    return dictionary
+Nick_Dict = markov_chain(Nick_text)
+Shakespeare_Dict = markov_chain(Shakespeare)
+
+def generator(chain, count):
+    w1 = random.choice(list(chain.keys()))
+    sentence = w1.capitalize()
+    for i in range(count - 1):
+        w1 = random.choice(chain[w1])
+        w2 = w1
+        sentence += ' ' + w2 
+    sentence += random.choice(['.', '?', '!', ';', '...'])
+    return sentence
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form.get("Generate") == "Generate":
+                return render_template("index.html", text=generator(Nick_Dict, 25))
+    else:
+        return render_template("index.html",text = generator(Nick_Dict, random.randint(10, 50)))
+if __name__ == "__main__":
+    app.run()
